b697afd3131f3e8f25ab75cae4b1872ed2ec040a
sarah-browning <browning.sarah.a@gmail.com>
2021-10-08 23:42:15 -0300

    Update GameProperties.java
    Added screen width, height and character step options and default values.

--- Invaders/src/GameProperties.java
+++ Invaders/src/GameProperties.java
@@ -1,4 +1,8 @@
 
 public class GameProperties {
-
+	public static final int SCREEN_WIDTH = 800;
+	public static final int SCREEN_HEIGHT = 600;
+	public static final int CHARACTER_STEP = 20;
+	
+	//TODO: Alter size based on necessary game size
 }
