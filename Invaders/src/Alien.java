cdf7ec4058d6658ce0fe33df9a8d23e05ac478a4
sarah-browning <browning.sarah.a@gmail.com>
2021-10-08 23:19:24 -0300

    Create Alien.java; Add sprite placeholder image
    Created skeleton of Alien.java and included placeholder. Still needs to have actual Sprite info added once sprites are finalized.

---
+++ Invaders/src/Alien.java
@@ -0,0 +1,7 @@
+
+public class Alien extends Sprite {
+	public Alien() {
+		super(x, y, "filename.png");
+		//TODO: call constructor from sprite with alien sprite width, height, filename once sprite finalized
+	}
+}
