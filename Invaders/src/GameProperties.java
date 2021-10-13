7ad063996e846ba1e25dcd1cfed84d2d8c7f1a56
sarah-browning <browning.sarah.a@gmail.com>
2021-10-12 22:06:23 -0300

    Add/Display background image in JFrame
    Added background image and edited SpaceInvadersPrep.java to display background. Minor edits to clean up comments on GameProperties.java.

--- Invaders/src/GameProperties.java
+++ Invaders/src/GameProperties.java
@@ -3,6 +3,4 @@ public class GameProperties {
 	public static final int SCREEN_WIDTH = 900;
 	public static final int SCREEN_HEIGHT = 650;
 	public static final int CHARACTER_STEP = 20;
-	
-	//TODO: Alter size based on necessary game size
 }
--- Invaders/src/SpaceInvadersPrep.java
+++ Invaders/src/SpaceInvadersPrep.java
@@ -1,19 +1,29 @@
+import java.awt.Color;
 import java.awt.Container;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class SpaceInvadersPrep extends JFrame {
 
 	private static final long serialVersionUID = 5224759096658742751L;
 	
 	//Memory Storage
-	private Alien[] myAliens;
+	private Alien myAliens;
 	private Starship myStarship;
+	private int[] aliensX = new int[55];
+	private int[] aliensY = new int[55];
+
 	
-	private JLabel StarshipLabel, AlienLabel;
-	private ImageIcon StarshipImage, AlienImage;
+	private JLabel BackgroundLabel, StarshipLabel, AlienLabel;
+	private ImageIcon BackgroundImage, AlienImage, StarshipImage;
 	
 	//Container to hold graphics
 	private Container content;
@@ -21,17 +31,38 @@ public class SpaceInvadersPrep extends JFrame {
 	//GUI Setup
 	public SpaceInvadersPrep() {
 		super("Space Invaders");
-		setSize(GameProperties.SCREEN_WIDTH, GameProperties.SCREEN_HEIGHT);		//TODO: Make sure to change to correct size in GameProperties.java
+		setSize(GameProperties.SCREEN_WIDTH, GameProperties.SCREEN_HEIGHT);
+		setResizable(false);
 		
-		//create new spaceship
+		BackgroundLabel = new JLabel();
+		BackgroundImage = new ImageIcon("./background.jpg");
+		BackgroundLabel.setIcon(BackgroundImage);
+		BackgroundLabel.setSize(GameProperties.SCREEN_WIDTH, GameProperties.SCREEN_HEIGHT);
+
+			//Alternate method of drawing images:
+			//		protected void paintComponent (Graphics g) {
+			//		BufferedImage background;
+			//		try {
+			//			background = ImageIO.read(new File("./background.jpg"));
+			//			g.drawImage(background, 0, 0, 900, 650, null);
+			//			
+			//		} catch (IOException e) {
+			//			e.printStackTrace();
+			//		}
+			//	}
+			//};
+		
+		
+		add(BackgroundLabel);
+		setSize(GameProperties.SCREEN_WIDTH, GameProperties.SCREEN_HEIGHT);
+		
+		//Create new spaceship
 		myStarship = new Starship();
 		StarshipLabel = new JLabel();
 		
-		//create new array of aliens
-		myAliens = new Alien[55];		//TODO: 
-		
 		//Exit game on window close
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
 	}
 	
 	//Main
