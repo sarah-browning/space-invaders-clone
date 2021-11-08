1a6b7ff98b4eb9bc74f1552a8fbbd073f593caf9
sarah-browning <browning.sarah.a@gmail.com>
2021-11-07 20:50:30 -0400

    Create JFrame

--- Invaders/src/SpaceInvadersPrep.java
+++ Invaders/src/SpaceInvadersPrep.java
@@ -1,11 +1,29 @@
 import javax.swing.JFrame;
 
 public class SpaceInvadersPrep extends JFrame implements Runnable {
-
-	@Override
-	public void run() {
-		// TODO Auto-generated method stub
-		
+	
+	//Attributes
+	private static final long serialVersionUID = 9121643710941167328L;
+	
+	//Prepare GUI
+	public SpaceInvadersPrep() {
+		super("Space Invaders");
+		setSize(GameProperties.SCREEN_WIDTH, GameProperties.SCREEN_HEIGHT);
+		setResizable(false);
+		setLayout(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
+	
+	@Override
+	public void run() {
+		
+	}
+	
+	//Main
+	public static void main( String[] args ) {
+		SpaceInvadersPrep game = new SpaceInvadersPrep();
+		game.setVisible(true);
+	}
+	
 }
