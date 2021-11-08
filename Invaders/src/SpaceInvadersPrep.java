6cc5ddc667bfc3142e26dc172977970be1f06fe8
sarah-browning <browning.sarah.a@gmail.com>
2021-11-07 20:51:21 -0400

    Create start and stop methods for thread

--- Invaders/src/SpaceInvadersPrep.java
+++ Invaders/src/SpaceInvadersPrep.java
@@ -4,6 +4,8 @@ public class SpaceInvadersPrep extends JFrame implements Runnable {
 	
 	//Attributes
 	private static final long serialVersionUID = 9121643710941167328L;
+	private Boolean running = false;
+	private Thread t;	
 	
 	//Prepare GUI
 	public SpaceInvadersPrep() {
@@ -13,7 +15,30 @@ public class SpaceInvadersPrep extends JFrame implements Runnable {
 		setLayout(null);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
-
+	
+	//Start the thread if not already running
+	private synchronized void start() {
+		if (running)
+			return;
+		
+		running = true;
+		t = new Thread(this);
+		t.start();
+	}
+	
+	//Stop the thread from running and exit the game
+	private synchronized void stop() {
+		if (!running) 
+			return;
+		
+		running = false;
+		try {
+			t.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.exit(1);
+	}
 	
 	@Override
 	public void run() {
@@ -24,6 +49,7 @@ public class SpaceInvadersPrep extends JFrame implements Runnable {
 	public static void main( String[] args ) {
 		SpaceInvadersPrep game = new SpaceInvadersPrep();
 		game.setVisible(true);
+		game.start();
 	}
 	
 }
